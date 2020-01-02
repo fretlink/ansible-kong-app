a3c4b3d5444e5d79afbd823a9acc12220a91f41f
Paul Bonaud <paul.bonaud@fretlink.com>
2020-01-02 15:25:06 +0100

    tests: fake a Kong server

---
+++ tests/kong.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(CGIHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/services/test':
+            self.send_response(404)
+        else:
+            self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(b'{ "data": [] }')
+
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+
+httpd = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
